ba229a06a4985da970ff57a6541a1bc979b7e402
Andy Casey <andycasey@gmail.com>
2019-04-11 11:59:15 -0400

    dont leave file pointers open

--- python/astra/__init__.py
+++ python/astra/__init__.py
@@ -33,7 +33,7 @@ config = yaml.load(open(os.path.dirname(__file__) + '/etc/{0}.yml'.format(NAME))
 # If there is a custom configuration file, updates the defaults using it.
 custom_config_fn = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
 if os.path.exists(custom_config_fn):
-    config = merge(yaml.load(open(custom_config_fn)), config)
-
+    with open(custom_config_fn) as fp:
+        config = merge(yaml.load(fp, Loader=yaml.FullLoader), config)
 
 __version__ = '0.1.0dev'
