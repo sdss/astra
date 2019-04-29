aec4c1f217a8c14c1f818e8bd684cd9afa886acc
Andy Casey <andycasey@gmail.com>
2019-04-29 09:30:58 -0400

    fixes

--- python/astra/__init__.py
+++ python/astra/__init__.py
@@ -3,8 +3,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
-
 import yaml
+from pkg_resources import parse_version
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(name).
@@ -28,14 +28,19 @@ NAME = 'astra'
 
 
 # Loads config
-config_path = os.path.dirname(__file__) + '/etc/{0}.yml'.format(NAME)
+yaml_kwds = dict()
+if parse_version(yaml.__version__) >= parse_version("5.1"):
+    yaml_kwds.update(Loader=yaml.FullLoader)
+
+config_path = os.path.join(os.path.dirname(__file__), 'etc/{0}.yml'.format(NAME))
 with open(config_path, "r") as fp:
-    config = yaml.load(fp, Loader=yaml.FullLoader)
+    config = yaml.load(fp, **yaml_kwds)
 
 # If there is a custom configuration file, updates the defaults using it.
-custom_config_fn = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
-if os.path.exists(custom_config_fn):
-    with open(custom_config_fn) as fp:
-        config = merge(yaml.load(fp, Loader=yaml.FullLoader), config)
+custom_config_path = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
+if os.path.exists(custom_config_path):
+    with open(custom_config_path, "r") as fp:
+        custom_config = yaml.load(custom_config_path, **yaml_kwds)
+    config = merge(custom_config, config)
 
 __version__ = '0.1.1dev'
