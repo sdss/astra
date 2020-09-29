853240fc087da105e166173576c95488d7330066
Andy Casey <andycasey@gmail.com>
2020-09-29 23:50:15 +0000

    util funcs

--- python/astra/utils/__init__.py
+++ python/astra/utils/__init__.py
@@ -3,3 +3,8 @@
 from sdsstools.logger import get_logger
 
 log = get_logger(__name__.split(".")[0])
+
+
+def unique_dicts(list_of_dicts):
+    return [dict(y) for y in set(tuple(x.items()) for x in list_of_dicts)]
+
