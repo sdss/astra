f9504c6d94b26d0528f8fe61022f58040ce914e8
Andy Casey <andycasey@gmail.com>
2020-10-26 17:22:38 +1100

    remove old code and add apogeenet as contrib

---
+++ python/astra/contrib/apogeenet/model.py
@@ -0,0 +1,85 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class Net(nn.Module):
+
+    def __init__(self, num_layers=1, num_targets=3, drop_p=0.0):
+        super(Net, self).__init__()
+        # 3 input channels, 6 output channels, convolution
+        # kernel
+        self.conv1 = nn.Conv1d(num_layers, 8, 3, padding=1)
+        self.conv2 = nn.Conv1d(8, 8, 3, padding=1)
+        self.conv3 = nn.Conv1d(8, 16, 3, padding=1)
+        self.conv4 = nn.Conv1d(16, 16, 3, padding=1)
+        self.conv5 = nn.Conv1d(16, 16, 3, padding=1)
+        self.conv6 = nn.Conv1d(16, 16, 3, padding=1)
+        self.conv7 = nn.Conv1d(16, 32, 3, padding=1)
+        self.conv8 = nn.Conv1d(32, 32, 3, padding=1)
+        self.conv9 = nn.Conv1d(32, 32, 3, padding=1)
+        self.conv10 = nn.Conv1d(32, 32, 3, padding=1)
+        self.conv11 = nn.Conv1d(32, 64, 3, padding=1)
+        self.conv12 = nn.Conv1d(64, 64, 3, padding=1)
+
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(64*133*1, 512)
+        self.fc1_dropout = nn.Dropout(p=drop_p)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, num_targets)
+
+
+    def forward(self, x):
+        x = F.max_pool1d(F.relu(self.conv2(F.relu(self.conv1(x)))), 2)
+        x = F.max_pool1d(F.relu(self.conv4(F.relu(self.conv3(x)))), 2)
+        x = F.max_pool1d(F.relu(self.conv6(F.relu(self.conv5(x)))), 2)
+        x = F.max_pool1d(F.relu(self.conv8(F.relu(self.conv7(x)))), 2)
+        x = F.max_pool1d(F.relu(self.conv10(F.relu(self.conv9(x)))), 2)
+        x = F.max_pool1d(F.relu(self.conv12(F.relu(self.conv11(x)))), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1_dropout(self.fc1(x)))
+        x = F.relu(self.fc1_dropout(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+
+def predict(model, eval_inputs):
+    
+    with torch.no_grad():
+        eval_outputs = model.forward(eval_inputs)
+
+    # Calculate mean values.
+    # TODO: These should not be hard-coded in! They should be stored with the model.
+    means = np.array([
+        2.880541250669337,
+        4716.915128138449,
+        -0.22329606176144642
+    ])
+    sigmas = np.array([
+        1.1648147820369943, # LOGG
+        733.0099523547299,  # TEFF
+        0.3004270650813916, # FE_H
+    ])
+
+    # Scale the outputs.
+    outputs = eval_outputs * sigmas + means
+    
+    param_names = ("logg", "teff", "fe_h")
+    result = dict(zip(param_names, torch.mean(outputs, 0).numpy()))
+    result.update(zip(
+        [f"u_{p}" for p in param_names],
+        torch.std(outputs, 0).numpy()
+    ))
+
+    return result
---
+++ python/astra/contrib/apogeenet/tasks.py
@@ -0,0 +1,272 @@
+
+import luigi
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import yaml
+from tqdm import tqdm
+from sdss_access import SDSSPath
+from astropy.table import Table
+from luigi.parameter import ParameterVisibility
+
+import astra
+from astra.tasks.base import BaseTask
+from astra.tasks.io import (ApStarFile, AllStarFile)
+from astra.tools.spectrum import Spectrum1D
+
+from astra.contrib.apogeenet.model import Net, predict
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class APOGEENetMixin(BaseTask):
+    """ A mixin class for APOGEENet tasks. """
+
+    task_namespace = "APOGEENet"
+
+    model_path = astra.Parameter(
+        description="The path of the trained APOGEENet model.",
+        always_in_help=True
+    )
+
+
+class TrainedAPOGEENetModel(APOGEENetMixin):
+
+    """ A trained APOGEENet model file. """
+
+    def output(self):    
+        return luigi.LocalTarget(self.model_path)
+
+
+class EstimateStellarParameters(APOGEENetMixin):
+    """
+    Estimate stellar parameters of a young stellar object, given a trained APOGEENet model.
+
+    :param model_path:
+        The path of the trained APOGEENet model.
+
+    :param uncertainty: (optional)
+        The number of draws to use when calculating the uncertainty in the
+        network (default: 100).
+    """
+
+    uncertainty = astra.IntParameter(
+        description="The number of draws to use when calculating the uncertainty in the network.",
+        default=100,
+        always_in_help=True,
+        visibility=ParameterVisibility.HIDDEN,
+    )
+
+    def output(self):
+        """ The output produced by this task. """
+        # Place relative to the observation path.
+        return luigi.LocalTarget(
+             os.path.join(
+                 os.path.dirname(self.input()["observation"].path), 
+                f"{self.task_id}.yaml"
+             )
+        )
+
+
+    def read_model(self):
+        """ Read in the trained APOGEENet model. """
+
+        # Load model.
+        model = Net()
+        model.load_state_dict(
+            torch.load(
+                self.input()["model"].path,
+                map_location=device
+            ),
+            strict=False
+        )
+        model.to(device)
+        model.eval()
+        return model
+
+
+    def read_observation(self):
+        """ Read in the observations. """
+        return Spectrum1D.read(self.input()["observation"].path)
+
+
+    def estimate_stellar_parameters(self, model, spectrum):
+        """
+        Estimate stellar parameters given a trained APOGEENet model and a spectrum.
+
+        :param model:
+            The APOGEENet model as a torch network.
+        
+        :param spectrum:
+            An observed spectrum.
+        """
+
+        n_pixels = spectrum.flux.shape[1]
+        
+        # Build flux tensor as described.
+        idx = 0
+        flux = torch.from_numpy(spectrum.flux.value[idx].astype(np.float32)).to(device)
+        error = torch.from_numpy(spectrum.uncertainty.array[idx]**-0.5).to(device)
+
+        flux_tensor = torch.randn(self.uncertainty, 1, n_pixels).to(device)
+
+        median_error = torch.median(error).item()
+        
+        error = torch.where(error == 1.0000e+10, flux, error)
+        error_t = torch.tensor([5 * median_error]).to(device)
+        error_t.repeat(n_pixels)
+        error = torch.where(error >= 5 * median_error, error_t, error)
+
+        flux_tensor = flux_tensor * error + flux
+        flux_tensor[0][0] = flux
+        
+        # Estimate quantities.
+        return predict(model, flux_tensor)
+
+
+    def write_output(self, result):        
+        """
+        Write stellar parameter estimates to the output location of this task.
+        """
+
+        sanitised_result = { k: float(v) for k, v in result.items() }
+        with open(self.output().path, "w") as fp:
+            yaml.dump(sanitised_result, fp)
+
+        self.trigger_event(luigi.Event.SUCCESS, self)
+        return None
+
+
+    def run(self):
+        """
+        Estimate stellar parameters given an APOGEENet model and ApStarFile(s).
+        """
+
+        model = self.read_model()
+
+        # This task can be run in batch mode.
+        for task in tqdm(self.get_batch_tasks()):
+
+            spectrum = task.read_observation()
+            result = task.estimate_stellar_parameters(model, spectrum)
+
+            task.write_output(result)
+        
+        return None
+
+
+class EstimateStellarParametersGivenApStarFile(EstimateStellarParameters, ApStarFile):
+
+    """
+    Estimate stellar parameters of a young stellar object, given a trained APOGEENet model and an ApStar file.
+
+    This task also requires all parameters that `astra.tasks.io.ApStarFile` requires.
+    **TODO**: Update this docstring once the parameters for `ApStarFile` in sdss5 are stable.
+
+    :param model_path:
+        The path of the trained APOGEENet model.
+
+    :param uncertainty: (optional)
+        The number of draws to use when calculating the uncertainty in the
+        network (default: 100).
+
+    """
+
+    def requires(self):
+        return {
+            "model": TrainedAPOGEENetModel(**self.get_common_param_kwargs(TrainedAPOGEENetModel)),
+            "observation": ApStarFile(**self.get_common_param_kwargs(ApStarFile))
+        }
+
+
+class EstimateStellarParametersForAllStarSpectra(APOGEENetMixin, AllStarFile):
+
+    release = luigi.Parameter()
+    apred = luigi.Parameter()
+    aspcap = luigi.Parameter()
+
+    use_remote = luigi.BoolParameter(
+        default=False, 
+        significant=False,
+        visibility=ParameterVisibility.HIDDEN
+    )
+    
+    def requires(self):
+        return AllStarFile(**self.get_common_param_kwargs(AllStarFile))
+    
+    def run(self):
+        
+        # Load in all the apStar files.
+        table = Table.read(self.input().path)
+        
+        def get_task(row):
+            return EstimateStellarParametersGivenApStarFile(
+                model_path=self.model_path,
+                release=self.release,
+                apred=self.apred,
+                telescope=row["TELESCOPE"],
+                field=row["FIELD"],
+                prefix=row["FILE"][:2],
+                obj=row["APOGEE_ID"],
+                apstar="stars",
+                use_remote=self.use_remote,
+            )
+            
+        # Load model
+        model = get_task(table[0]).read_model()
+
+        N = len(table)
+        for i, row in enumerate(table):
+
+            task = get_task(row)
+            if task.complete():
+                continue
+
+            spectrum = task.read_observation()
+            result = task.estimate_stellar_parameters(model, spectrum)
+
+            task.write_output(result)
+
+            try:
+                self.set_progress_percentage(100 * i / N)
+            except TypeError:
+                None
+
+            # Generate the task just so Luigi central scheduler can see that it gets done.
+            #yield task            
+
+        return None
+
+
+
+if __name__ == "__main__":
+
+    #/home/andy/data/sdss/dr16/apogee/spectro/redux/r12/stars/apo25m/218-04/apStar-r12-2M06440890-0610126.fits
+
+    '''
+    task = EstimateStellarParameters(
+        model_path="../../../APOGEE_NET.pt",
+        release="dr16",
+        apred="r12",
+        telescope="apo25m", 
+        field="218-04",
+        prefix="ap",
+        obj="2M06440890-0610126",
+        apstar="stars"
+    )
+    '''
+    task = EstimateStellarParametersForAllStarSpectra(
+        model_path="../../../APOGEE_NET.pt",
+        release="dr16",
+        apred="r12",
+        aspcap="l33",
+        use_remote=True
+    )
+
+    luigi.build(
+        [task],
+        detailed_summary=True
+    )
+    #task.run()
+
