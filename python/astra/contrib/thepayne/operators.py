continuum normalization and fixing some operator logic

python/astra/contrib/thepayne/operators.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import numpy as np
 from tqdm import tqdm
 from sdss_access import SDSSPath
 from inspect import signature
@@ -7,9 +8,11 @@ from collections import OrderedDict
 
 from astra.database import astradb, session
 from astra.tools.spectrum import Spectrum1D
-from astra.database.utils import create_task_output
+from astra.database.utils import create_task_output, deserialize_pks
 from astra.contrib.thepayne import training, test
 from astra.utils import hashify, log, get_base_output_path
+from astra.operators import ApStarOperator, AstraOperator
+from astra.operators.utils import prepare_data
 
 def get_model_path(
         training_set_path: str,
@@ -128,7 +131,6 @@ def train_model(
 def estimate_stellar_labels(
         pks,
         model_path,
-        analyze_individual_visits=True,
     ):
     """
     Estimate stellar labels given a single-layer neural network.
@@ -146,63 +148,42 @@ def estimate_stellar_labels(
         data product, which is usually the stacked spectrum.
     """
     log.info(f"Loading model from {model_path}")
+    
     state = test.load_state(model_path)
 
     label_names = state["label_names"]
     L = len(label_names)
     log.info(f"Estimating these {L} label names: {label_names}")
 
-    pks = deserialize_pks(pks)
-    N = len(pks)
+    log.info(f"Estimating stellar labels for task instances")
 
-    log.info(f"Estimating stellar labels for {N} task instances")
-
-    sliced = slice(0, 1) if analyze_individual_visits else slice(None)
-
-    trees = {}
     results = {}
-    for pk in tqdm(pks, desc="The Payne"):
+    for instance, path, spectrum in prepare_data(pks):
+        if spectrum is None: continue
 
-        q = session.query(astradb.TaskInstance).filter(astradb.TaskInstance.pk == pk)
-
-        instance = q.one_or_none()
-        if instance is None:
-            log.warning(f"No task instance found for primary key {pk}")
-            continue
-        
-        # Get the path.
-        parameters = instance.parameters
-        tree = trees.get(parameters["release"], None)
-        if tree is None:
-            trees[parameters["release"]] = tree = SDSSPath(release=parameters["release"])
-        
-        path = tree.full(**parameters)
-
-        # Load spectrum.
-        try:
-            spectrum = Spectrum1D.read(path)
-        except:
-            log.exception(f"Unable to load spectrum from {path} for task instance {instance}")
-            continue
-        
         # Run optimization.
         p_opt, p_cov, model_flux, meta = test.test(
             spectrum.wavelength.value,
-            spectrum.flux.value[sliced],
-            spectrum.uncertainty.array[sliced],
+            spectrum.flux.value,
+            spectrum.uncertainty.array,
             **state
         )
+        
+        log.debug(f"spectrum shape: {spectrum.flux.shape}")
+        log.debug(f"p_opt shape: {p_opt.shape}")
+        log.debug(f"spectrum meta: {spectrum.meta['snr']}")
 
         # Prepare outputs.
         result = dict(zip(label_names, p_opt.T))
-        result.update(snr=spectrum.meta["snr"][sliced])
+        result.update(snr=spectrum.meta["snr"])
         # Include uncertainties.
         result.update(dict(zip(
             (f"u_{ln}" for ln in label_names),
-            np.sqrt(p_cov[:, np.arange(L), np.arange(L)].T)
+            np.sqrt(p_cov[:, np.arange(p_opt.shape[1]), np.arange(p_opt.shape[1])].T)
         )))
         
         results[instance.pk] = result
+        log.info(f"Result for {instance}: {result}")
 
     # Write database outputs.
     for pk, result in tqdm(results.items(), desc="Writing database outputs"):
@@ -210,3 +191,47 @@ def estimate_stellar_labels(
         create_task_output(pk, astradb.ThePayne, **result)
 
     return None
+
+
+class TrainThePayneOperator(AstraOperator):
+
+    def __init__(
+        self,
+        output_model_path,
+        training_set_path,
+        num_epochs=100_000,
+        num_neurons=300,
+        weight_decay=0.0,
+        learning_rate=0.001,
+        **kwargs,
+    ) -> None:
+        super(TrainThePayneOperator, self).__init__(**kwargs)
+        self.output_model_path = output_model_path
+        self.training_set_path = training_set_path
+        self.num_epochs = num_epochs
+        self.num_neurons = num_neurons
+        self.weight_decay = weight_decay
+        self.learning_rate = learning_rate
+        
+        self.bash_command_prefix = "astra run thepayne train"
+        self.python_callable = train_model
+
+        return None
+
+    
+
+class ThePayneApStarOperator(ApStarOperator):
+
+    template_fields = ("model_path", )
+
+    def __init__(
+        self,
+        model_path: str,
+        **kwargs,
+    ) -> None:
+        super(ThePayneApStarOperator, self).__init__(**kwargs)
+        self.model_path = model_path
+        self.python_callable = estimate_stellar_labels
+        self.bash_command_prefix = "astra run thepayne test"
+
+        return None

python/astra/tools/continuum/sinusoids.py
@@ -4,16 +4,11 @@ Continuum-normalization using sines and cosines.
 
 import numpy as np
 import os
-from warnings import warn
 
-from astropy.units import Quantity
-from astropy.nddata.nduncertainty import InverseVariance 
-
+from astra.utils import log
 
 def normalize(
-        dispersion,
-        flux, 
-        ivar, 
+        spectrum,
         continuum_regions=None, 
         L=1400, 
         order=3, 
@@ -25,16 +20,8 @@ def normalize(
     Pseudo-continuum-normalize the flux using a defined set of continuum pixels and a sum of sine 
     and cosine functions.
 
-    :param dispersion:
-        The dispersion values.
-
-    :param flux:
-        The flux values for all pixels, as they correspond to the `dispersion`
-        array.
-
-    :param ivar:
-        The inverse variances for all pixels, as they correspond to the
-        `dispersion` array.
+    :param spectrum:
+        The `Spectrum1D` object to normalize.
 
     :param continuum_regions: [optional]
         A list of two-length tuples that describe the start and end of regions
@@ -68,12 +55,18 @@ def normalize(
         metadata about the fit.
     """
 
-    if False and continuum_regions is None:
-        default_path = os.path.join(os.path.dirname(__file__), "etc/continuum-regions.list")
+    if continuum_regions is None:
+        default_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "../../etc/continuum-regions.list"
+        )
         continuum_regions = np.loadtxt(default_path)
 
     elif isinstance(continuum_regions, str):
         continuum_regions = np.loadtxt(continuum_regions)
+
+    dispersion = spectrum.wavelength.value
+    flux, ivar = (spectrum.flux.value, spectrum.uncertainty.array)
 
     # Work out the continuum pixels.
     mask = np.zeros(dispersion.size, dtype=bool)
@@ -98,15 +91,43 @@ def normalize(
         **kwargs
     )
 
-    normalized_flux = flux/continuum
-    normalized_ivar = continuum * ivar * continuum
-    normalized_flux[normalized_ivar == 0] = 1.0
-    
-    non_finite_pixels = ~np.isfinite(normalized_flux)
-    normalized_flux[non_finite_pixels] = 1.0
-    normalized_ivar[non_finite_pixels] = 0.0
+    spectrum._data /= continuum
+    spectrum._uncertainty.array *= continuum * continuum
 
-    return (normalized_flux, normalized_ivar, continuum, metadata)
+    no_information = (spectrum._uncertainty.array == 0)
+    spectrum._data[no_information] = 1.0
+
+    non_finite_pixels = ~np.isfinite(spectrum._data)
+    spectrum._data[non_finite_pixels] = 1.0
+    spectrum._uncertainty.array[non_finite_pixels] = 0.0
+
+    if "slice_args" in kwargs or "shape" in kwargs:
+        return slice_and_shape(spectrum, **kwargs)
+    return spectrum
+
+
+
+def slice_and_shape(spectrum, slice_args, shape, repeat=None, **kwargs):
+
+    if repeat is not None:
+        spectrum._data = np.repeat(spectrum._data, repeat)
+        spectrum._uncertainty.array = np.repeat(spectrum._uncertainty.array, repeat)
+
+    if slice_args is not None:
+        slices = tuple([slice(*each) for each in slice_args])
+
+        spectrum._data = spectrum._data[slices]
+        spectrum._uncertainty.array = spectrum._uncertainty.array[slices]
+
+        try:
+            spectrum.meta["snr"] = spectrum.meta["snr"][slices[0]]
+        except:
+            log.warning(f"Unable to slice 'snr' metadata with {slice_args}")
+
+    spectrum._data = spectrum._data.reshape(shape)
+    spectrum._uncertainty.array = spectrum._uncertainty.array.reshape(shape)
+
+    return spectrum
 
 
 def sines_and_cosines(
@@ -223,8 +244,8 @@ def sines_and_cosines(
             segments = ", ".join(["{:.1f} to {:.1f}".format(
                 dispersion[s], dispersion[e], e-s) for s, e in segment_indices])
 
-            warn(f"Some pixels in have measured flux values (e.g., ivar > 0) but are not included "
-                 f"in any specified region ({segments}).")
+            log.warning(f"Some pixels in have measured flux values (e.g., ivar > 0) but "
+                        f"are not included in any specified region ({segments}).")
 
         # Get the flux and inverse variance for this object.
         object_metadata = []
@@ -291,3 +312,14 @@ def _continuum_design_matrix(
             [np.cos(o * scale * dispersion), np.sin(o * scale * dispersion)] \
             for o in range(1, order + 1)]).reshape((2 * order, dispersion.size))
         ])
+
+
+if __name__ == "__main__":
+
+    #path = '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/mwm/apogee/spectro/redux/daily/stars/apo25m/97/97480/apStar-daily-apo25m-2M21241108+0023308.fits'
+    path = path = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/mwm/apogee/spectro/redux/daily/stars/apo25m/56/56822/apStar-daily-apo25m-2M11322307+2500207-59314.fits"
+    from astra.tools.spectrum import Spectrum1D
+    spec = Spectrum1D.read(path)
+
+
+    normalize(spec)