3f3dcb16d9574ebb26b327c3b4fbd91fd197223a
Andy Casey <andycasey@gmail.com>
2022-02-24 19:00:25 -0700

    use db url if given

--- python/astra/database/astradb.py
+++ python/astra/database/astradb.py
@@ -1,5 +1,7 @@
 import json
-from peewee import AutoField, TextField, ForeignKeyField, DateTimeField
+from functools import lru_cache
+from sdss_access import SDSSPath
+from peewee import (SqliteDatabase, AutoField, TextField, ForeignKeyField, DateTimeField, BigIntegerField, FloatField, BooleanField)
 from sdssdb.connection import PeeweeDatabaseConnection
 from sdssdb.peewee import BaseModel
 from astra import (config, log)
@@ -7,37 +9,46 @@ from astra import (config, log)
 # The database config should always be present, but let's not prevent importing the module because it's missing.
 _database_config = config.get("astra_database", {})
 
-class AstraDatabaseConnection(PeeweeDatabaseConnection):
-    dbname = _database_config.get("dbname", None)
-    
-database = AstraDatabaseConnection(autoconnect=True)
+# If a URL is given, that overrides all other config settings.
+_database_url = _database_config.get("url", None)
+if _database_url:
+    from playhouse.db_url import connect
+    database = connect(_database_url)
+    schema = None
 
-profile = _database_config.get("profile", None)
-if profile is not None:
-    try:
-        database.set_profile(profile)
-    except AssertionError as e:
-        log.exception(e)
-        log.warning(f"""
-        Database profile '{profile}' set in Astra configuration file, but there is no database 
-        profile called '{profile}' found in ~/.config/sdssdb/sdssdb.yml -- it should look like:
+else:
+    class AstraDatabaseConnection(PeeweeDatabaseConnection):
+        dbname = _database_config.get("dbname", None)
         
-        {profile}:
-            user: [USER]
-            host: [HOST]
-            port: 5432
-            domain: [DOMAIN]
+    database = AstraDatabaseConnection(autoconnect=True)
+    schema = _database_config.get("schema", None)
 
-        See https://sdssdb.readthedocs.io/en/stable/intro.html#supported-profiles for more details. 
-        If the profile name '{profile}' is incorrect, you can change the 'database' / 'profile' key 
-        in ~/.astra/astra.yml
-        """)
+    profile = _database_config.get("profile", None)
+    if profile is not None:
+        try:
+            database.set_profile(profile)
+        except AssertionError as e:
+            log.exception(e)
+            log.warning(f"""
+            Database profile '{profile}' set in Astra configuration file, but there is no database 
+            profile called '{profile}' found in ~/.config/sdssdb/sdssdb.yml -- it should look like:
+            
+            {profile}:
+                user: [USER]
+                host: [HOST]
+                port: 5432
+                domain: [DOMAIN]
+
+            See https://sdssdb.readthedocs.io/en/stable/intro.html#supported-profiles for more details. 
+            If the profile name '{profile}' is incorrect, you can change the 'database' / 'profile' key 
+            in ~/.astra/astra.yml
+            """)
 
 
 class AstraBaseModel(BaseModel):
     class Meta:
         database = database
-        schema = _database_config.get("schema", None)
+        schema = schema
 
 
 class JSONField(TextField):
@@ -49,11 +60,36 @@ class JSONField(TextField):
             return json.loads(value)
         
 
+@lru_cache
+def _lru_sdsspath(release):
+    return SDSSPath(release=release)
+
+
+class Source(AstraBaseModel):
+    catalogid = BigIntegerField(primary_key=True)
+
+    # TODO: Include things like Gaia / 2MASS photometry?
+    # TODO: Do we even need these two?
+    sdssv_target0 = BigIntegerField(null=True)
+    sdssv_first_carton_name = TextField(null=True)
+
+    @property
+    def data_products(self):
+        return (
+            DataProduct.select()
+                       .join(SourceDataProduct)
+                       .join(Source)
+                       .where(Source.catalogid == self.catalogid)
+        )        
+
+
 class DataProduct(AstraBaseModel):
     pk = AutoField()
     release = TextField()
     filetype = TextField()
     kwargs = JSONField()
+
+    metadata = JSONField(null=True)
 
     class Meta:
         indexes = (
@@ -69,13 +105,35 @@ class DataProduct(AstraBaseModel):
                 .where(DataProduct.pk == self.pk)
         )
 
+    @property
+    def path(self):
+        kwds = self.kwargs.copy()
+        if "field" in kwds:
+            kwds["field"] = str(kwds["field"]).strip()
+        return _lru_sdsspath(self.release).full(self.filetype, **kwds)
+        
+
+    @property
+    def sources(self):
+        return (
+            Source.select()
+                  .join(SourceDataProduct)
+                  .join(DataProduct)
+                  .where(DataProduct.pk == self.pk)
+        )
+
+# DataProducts and Sources should be a many-to-many relationship.
+class SourceDataProduct(AstraBaseModel):
+    pk = AutoField()
+    source = ForeignKeyField(Source)
+    data_product = ForeignKeyField(DataProduct)
+
 
 class Task(AstraBaseModel):
     pk = AutoField()
     name = TextField()
     parameters = JSONField(null=True)
 
-    git_hash = TextField(null=True)
     version = TextField(null=True)
 
     # We want some times to be recorded:
@@ -134,9 +192,8 @@ class TaskOutputDataProducts(AstraBaseModel):
 
 def create_tables(drop_existing_tables=False):
     """ Create all tables for the Astra database. """
-    database.connect()
+    database.connect(reuse_if_open=True)
     models = AstraBaseModel.__subclasses__()
     if drop_existing_tables:
         database.drop_tables(models)
     database.create_tables(models)
-    database.close()
