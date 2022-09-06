40a6e74a369abbf98cb9eb2795b81fd38bcc7d49
Andy Casey <andycasey@gmail.com>
2022-09-05 23:27:37 -0600

    update spectrum loaders to be fast

--- python/astra/tools/spectrum/__init__.py
+++ python/astra/tools/spectrum/__init__.py
@@ -1,9 +1,1 @@
 from astra.tools.spectrum.loaders import Spectrum1D, SpectrumList
-from numpy import median
-
-
-def calculate_snr(spectrum, aggregate=median):
-    use = (spectrum.uncertainty.array > 0) & (spectrum.flux.value > 0)
-    flux = spectrum.flux.value[use]
-    sigma = spectrum.uncertainty.array[use] ** 0.5
-    return aggregate(flux / sigma)
