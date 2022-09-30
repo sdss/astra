298a73ce600db428cf0a2ed8a707a56c2182ae57
Andy Casey <andycasey@gmail.com>
2022-09-29 22:43:51 -0600

    version bump

--- python/astra/__init__.py
+++ python/astra/__init__.py
@@ -1,5 +1,5 @@
 NAME = "astra"
-__version__ = "0.2.4dev"
+__version__ = "0.2.5"
 
 # TODO: Move these things elsewhere (e.g., utils) to speed up top-level import.
 from sdsstools.configuration import get_config
