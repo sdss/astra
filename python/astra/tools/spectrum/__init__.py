d53aa6a4c6ae341dd94841a44f04e96e0cec7c6f
Andy Casey <andycasey@gmail.com>
2022-09-30 11:43:58 +1000

    AAHG

--- python/astra/tools/spectrum/__init__.py
+++ python/astra/tools/spectrum/__init__.py
@@ -1,2 +1,4 @@
 """Common tools for reading and writing many types of spectra."""
 from astra.tools.spectrum.readers import SpectralAxis, Spectrum1D, SpectrumList
+
+__all__ = ["Spectrum1D", "SpectrumList", "SpectralAxis"]
