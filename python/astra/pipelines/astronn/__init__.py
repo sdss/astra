9ef1b7793575b1a950ea0b26c96729e09933c867
Yingyi Song <u6039136@notchpeak21.int.chpc.utah.edu>
2023-09-27 20:18:44 -0600

    modify astronn according to the style on Aug 27, 2023 (8e24575)

--- python/astra/pipelines/astronn/__init__.py
+++ python/astra/pipelines/astronn/__init__.py
@@ -1,14 +1,16 @@
-from typing import Iterable
+from typing import Iterable, Optional
 import tensorflow_probability as tfp
 from astra import task
 from astra.models.astronn import AstroNN
 from astra.pipelines.astronn.utils import read_model
-from astra.pipelines.astronn.base import _worker, parallel_batch_read, _inference
+from astra.pipelines.astronn.base import _prepare_data, _worker, parallel_batch_read, _inference
 
 @task
 def astronn(
     spectra: Iterable,
     model_path: str = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/users/u6039136/software/git/astroNN_projects/astroNN_APOGEE_VAC/astroNN_retrain_2_shi/",
+    #model_path: str = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/users/u6039136/software/git/astroNN_projects/astroNN_APOGEE_VAC/astroNN_0617_run001/",
+    parallel: Optional[bool] = False,
 ) -> Iterable[AstroNN]:
     """
     Estimate astrophysical parameters for a stellar spectrum given a pre-trained neural network.
@@ -16,5 +18,13 @@ def astronn(
 
     model = read_model(model_path)
 
-    for batch in parallel_batch_read(_worker, spectra, batch_size=100, cpu_count=4):
-        yield from _inference(model, batch)
+    if parallel: # work for pipelines.sdss.org
+        for batch in parallel_batch_read(_worker, spectra, batch_size=100, cpu_count=4):
+            yield from _inference(model, batch)
+    else:
+        try:
+            for spectrum in spectra.iterator():
+                yield from _inference(model, [_prepare_data(spectrum)])
+        except:
+            for spectrum in spectra:
+                yield from _inference(model, [_prepare_data(spectrum)])
--- python/astra/pipelines/astronn/base.py
+++ python/astra/pipelines/astronn/base.py
@@ -1,7 +1,7 @@
 import numpy as np
 import multiprocessing as mp
 from itertools import cycle
-from time import time
+from time import time, sleep
 
 from astra.utils import log, flatten
 from astra.models.astronn import AstroNN
@@ -11,14 +11,18 @@ from astroNN.apogee import apogee_continuum
 
 
 def _prepare_data(spectrum):
-    N, P = np.atleast_2d(spectrum.flux).shape
-    flux = np.nan_to_num(spectrum.flux).astype(np.float32).reshape((N, P))
-    e_flux = np.nan_to_num(spectrum.ivar**-0.5).astype(np.float32).reshape((N, P))
-    bitmask = np.nan_to_num(spectrum.pixel_flags).astype(bool).reshape((N, P))
+    try:
+        N, P = np.atleast_2d(spectrum.flux).shape
+        flux = np.nan_to_num(spectrum.flux).astype(np.float32).reshape((N, P))
+        e_flux = np.nan_to_num(spectrum.ivar**-0.5).astype(np.float32).reshape((N, P))
+        bitmask = np.nan_to_num(spectrum.pixel_flags).astype(bool).reshape((N, P))
+    except:
+        return (spectrum.spectrum_id, spectrum.source_id, None, None)
 
     #### continuum normalization
-    norm_flux = np.zeros((N, 7514))
-    norm_flux_err = np.zeros((N, 7514))
+    P_new = 7514
+    norm_flux = np.zeros((N, P_new))
+    norm_flux_err = np.zeros((N, P_new))
     for i in range(N):
         norm_flux[i, :], norm_flux_err[i, :] = apogee_continuum(flux[i, :], e_flux[i, :], bitmask=bitmask[i, :], dr = 17)
 
@@ -32,14 +36,22 @@ def _worker(q_input, q_output):
 
         try:
             result = _prepare_data(spectrum)
-
-            while True:
-                if q_output.empty():
-                    q_output.put(result)
-                    break
         except:
             log.exception(f"Exception in worker with data product {spectrum}")
             continue
+        else:
+            # Only put results when the queue is empty, otherwise we might load data faster than we can process it.
+            # (which eventually leads to an out-of-memory error)
+
+            # Note from A. Casey: I commented this out while dealing with some weird COMMIT / GPU hanging, and I dn't know what the cause was.
+            #       If you get out of memory errors in future, maybe uncomment these.
+            while True:
+                if q_output.empty():
+                    q_output.put(result)
+                    log.info(f"Put result {result}")
+                    break
+
+                sleep(1)
 
     q_output.put(None)
     return None
@@ -52,89 +64,99 @@ def _inference(model, batch):
     t_init = time()
     spectrum_ids, source_ids, all_flux, all_e_flux = ([], [], [], [])
     for spectrum_id, source_id, f, e_f in batch:
-        spectrum_ids.append(spectrum_id)
-        source_ids.append(source_id)
-        all_flux.append(f)
-        all_e_flux.append(e_f)
-
-    N_param = len(model.targetname)
-
-    #### astroNN prediction: K-band absolute magnitude and distance
-    results = []
-    for i in range(len(batch)):
-        result = []
-        try:
-            pred, pred_err = model.predict(all_flux[i])
-            #print(pred, pred_err)
-        except ValueError:
-            result = [np.nan]*N_param*2 + [1]
+        if f is None:
+            # OS Error when loading it
+            yield AstroNN(
+                spectrum_id=spectrum_id,
+                source_id=source_id,
+                flag_no_result = True
+            )
         else:
-            for idx in range(N_param):
-                result.append(pred[0, idx])
-                result.append(pred_err['total'][0, idx])
-            result.append(0)
+            spectrum_ids.append(spectrum_id)
+            source_ids.append(source_id)
+            all_flux.append(f)
+            all_e_flux.append(e_f)
 
-        result = np.array(result)
-        print("+"*6, result)
-        results.append(result)
-    results = np.array(results)
-    print("+"*6, results.shape)
+    if len(all_flux) > 0:
+        N_param = len(model.targetname)
+
+        #### astroNN prediction 
+        results = []
+        for i in range(len(batch)):
+            result = []
+            try:
+                pred, pred_err = model.predict(all_flux[i])
+                #print(pred, pred_err)
+            except ValueError:
+                result = [np.nan]*N_param*2 + [1]
+            else:
+                for idx in range(N_param):
+                    result.append(pred[0, idx])
+                    result.append(pred_err['total'][0, idx])
+                result.append(0)
+
+            result = np.array(result)
+            print("+"*6, 'result:', result)
+            results.append(result)
+        results = np.array(results)
+        print("+"*6, 'shape of results:', results.shape)
     
-    #### record results
-    mean_t_elapsed = (time() - t_init) / len(spectrum_ids)
-    for i, (spectrum_id, source_id) in enumerate(zip(spectrum_ids, source_ids)):
-        output = AstroNN(
-            spectrum_id=spectrum_id,
-            source_id=source_id,
-            t_elapsed=mean_t_elapsed,
-            teff = results[i, 0],
-            e_teff = results[i, 1],
-            logg = results[i, 2],
-            e_logg = results[i, 3],
-            c_h = results[i, 4],
-            e_c_h = results[i, 5],
-            c_1_h = results[i, 6],
-            e_c_1_h = results[i, 7],
-            n_h = results[i, 8],
-            e_n_h = results[i, 9],
-            o_h = results[i, 10],
-            e_o_h = results[i, 11],
-            na_h = results[i, 12],
-            e_na_h = results[i, 13],
-            mg_h = results[i, 14],
-            e_mg_h = results[i, 15],
-            al_h = results[i, 16],
-            e_al_h = results[i, 17],
-            si_h = results[i, 18],
-            e_si_h = results[i, 19],
-            p_h = results[i, 20],
-            e_p_h = results[i, 21],
-            s_h = results[i, 22],
-            e_s_h = results[i, 23],
-            k_h = results[i, 24],
-            e_k_h = results[i, 25],
-            ca_h = results[i, 26],
-            e_ca_h = results[i, 27],
-            ti_h = results[i, 28],
-            e_ti_h = results[i, 29],
-            ti_2_h = results[i, 30],
-            e_ti_2_h = results[i, 31],
-            v_h = results[i, 32],
-            e_v_h = results[i, 33],
-            cr_h = results[i, 34],
-            e_cr_h = results[i, 35],
-            mn_h = results[i, 36],
-            e_mn_h = results[i, 37],
-            fe_h = results[i, 38],
-            e_fe_h = results[i, 39],
-            co_h = results[i, 40],
-            e_co_h = results[i, 41],
-            ni_h = results[i, 42],
-            e_ni_h = results[i, 43],
-            result_flags=results[i, 44]
-        )
-        #output.apply_flags(meta[i])
-        yield output
+        #### record results
+        mean_t_elapsed = (time() - t_init) / len(spectrum_ids)
+        for i, (spectrum_id, source_id) in enumerate(zip(spectrum_ids, source_ids)):
+            print("+"*6, 'write to database: Teff =', results[i, 0])
+            output = AstroNN(
+                spectrum_id=spectrum_id,
+                source_id=source_id,
+                t_elapsed=mean_t_elapsed,
+                teff = results[i, 0],
+                e_teff = results[i, 1],
+                logg = results[i, 2],
+                e_logg = results[i, 3],
+                c_h = results[i, 4],
+                e_c_h = results[i, 5],
+                c_1_h = results[i, 6],
+                e_c_1_h = results[i, 7],
+                n_h = results[i, 8],
+                e_n_h = results[i, 9],
+                o_h = results[i, 10],
+                e_o_h = results[i, 11],
+                na_h = results[i, 12],
+                e_na_h = results[i, 13],
+                mg_h = results[i, 14],
+                e_mg_h = results[i, 15],
+                al_h = results[i, 16],
+                e_al_h = results[i, 17],
+                si_h = results[i, 18],
+                e_si_h = results[i, 19],
+                p_h = results[i, 20],
+                e_p_h = results[i, 21],
+                s_h = results[i, 22],
+                e_s_h = results[i, 23],
+                k_h = results[i, 24],
+                e_k_h = results[i, 25],
+                ca_h = results[i, 26],
+                e_ca_h = results[i, 27],
+                ti_h = results[i, 28],
+                e_ti_h = results[i, 29],
+                ti_2_h = results[i, 30],
+                e_ti_2_h = results[i, 31],
+                v_h = results[i, 32],
+                e_v_h = results[i, 33],
+                cr_h = results[i, 34],
+                e_cr_h = results[i, 35],
+                mn_h = results[i, 36],
+                e_mn_h = results[i, 37],
+                fe_h = results[i, 38],
+                e_fe_h = results[i, 39],
+                co_h = results[i, 40],
+                e_co_h = results[i, 41],
+                ni_h = results[i, 42],
+                e_ni_h = results[i, 43],
+                result_flags=results[i, 44]
+            )
+            #output.apply_flags(meta[i])
+            yield output
 
 def parallel_batch_read(target, spectra, batch_size, cpu_count=None):
     """
@@ -165,17 +187,25 @@ def parallel_batch_read(target, spectra, batch_size, cpu_count=None):
         p = mp.Process(target=target, args=(q, q_output))
         p.start()
         qps.append((q, p))
-    
+
+    log.info(f"Distributing spectra")
     B, batch = (0, [])
-    for i, ((q,p), spectrum) in enumerate(zip(cycle(qps), flatten(spectra))):
+    for i, ((q, p), spectrum) in enumerate(zip(cycle(qps), flatten(spectra))):
         q.put(spectrum)
 
     for (q, p) in qps:
         q.put(None)
+    log.info(f"Done")
 
     N_done = 0
     while True:
-        result = q_output.get()
+        try:
+            result = q_output.get(timeout=30)
+        except:
+            log.exception("Timeout on thing")
+            continue
+        log.info(f"Have result {result}, {B}")
+
         if result is None:
             N_done += 1
             if N_done == N:
@@ -183,12 +213,12 @@ def parallel_batch_read(target, spectra, batch_size, cpu_count=None):
         else:
             batch.append(result)
             B += 1
-            if B == batch_size:
+            if B >= batch_size:
                 yield batch
                 B, batch = (0, [])
-    
+
     if batch:
         yield batch
     
-    for (q, p) in qps:
+    for q, p in qps:
         p.join()
