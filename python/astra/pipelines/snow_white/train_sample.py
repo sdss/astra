0b6637ffc79ca0f4042bd2d89ce54a2ff36416df
NPGFusillo <np.gentilefusillo@gmail.com>
2024-01-05 12:36:21 +0100

    updated train_sample.py and get_all_line_info_v3.py with updated labels

--- python/astra/pipelines/snow_white/train_sample.py
+++ python/astra/pipelines/snow_white/train_sample.py
@@ -6,16 +6,23 @@ from astra.utils import expand_path
 
 PIPELINE_DATA_DIR = expand_path("$MWM_ASTRA/pipelines/snow_white")
 
-def train_classifier(n_jobs=1, input_dataset_basename="file_for_training.csv", output_basename="training_file_LL"):
+def train_classifier(n_jobs=1, input_dataset_basename="file_for_training.csv", output_basename="training_file"):
 
     dataset = pandas.read_csv(os.path.join(PIPELINE_DATA_DIR, input_dataset_basename))#'additional_results'
     array = dataset.values
-    X = array[:,2:1806]
+    X = array[:,2:1753]
     Y = array[:,1]
-
-    knn = RandomForestClassifier(n_estimators=500, criterion="log_loss",class_weight="balanced",min_samples_split=4,min_samples_leaf=3,n_jobs=n_jobs,max_features=None,bootstrap=True,verbose=2)
+    knn = RandomForestClassifier(n_estimators=500, criterion="log_loss",class_weight="balanced",min_samples_split=4,min_samples_leaf=2,n_jobs=20,max_features=None,bootstrap=True,verbose=0,max_samples=1000)
     weights=[]
-    #for xxx in range(np.size(Y)):
+    for xxx in range(np.size(Y)):
+        if Y[xxx]=="CV":
+            weights.append(10)
+        elif Y[xxx]=="DA_MS":
+            weights.append(0.5)
+        elif Y[xxx]=="DB_MS":
+            weights.append(0.5)
+        else:
+            weights.append(2)
         #print("here...")
         #if Y[xxx]=="DBAZ" or Y[xxx]=="DABZ" or Y[xxx]=="DBZ" or Y[xxx]=="DAZ"or Y[xxx]=="DBA" or Y[xxx]=="DAe" or Y[xxx]=="DAH":
         #   weights.append(5)
