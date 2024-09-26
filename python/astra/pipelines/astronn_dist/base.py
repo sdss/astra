5363cb5475bd15eaff3d3a216bc4eeadf3eccbb8
Yingyi Song <songyingyi@gmail.com>
2024-09-26 13:58:20 -0600

    astronn_dist addes weighted_dist

--- python/astra/pipelines/astronn_dist/base.py
+++ python/astra/pipelines/astronn_dist/base.py
@@ -9,7 +9,15 @@ from astra.models.astronn_dist import AstroNNdist
 
 import tensorflow_probability as tfp
 from astroNN.apogee import apogee_continuum 
-from astroNN.gaia import extinction_correction, fakemag_to_pc
+from astroNN.gaia import extinction_correction, fakemag_to_pc, fakemag_to_parallax
+
+try:
+    from zero_point import zpt
+
+    zpt.load_tables()
+    ZPT = True
+except ImportError:
+    ZPT = False
 
 
 def _prepare_data(spectrum):
@@ -31,6 +39,12 @@ def _prepare_data(spectrum):
     #### get photometry and extinction
     meta, missing_photometry, missing_extinction = get_metadata(spectrum)
     #meta = np.tile(metadata, N).reshape((N, -1))
+
+    ### get zero-point corrected gaia parallax and error
+    parallax, parallax_err, zp = get_gaiadata(spectrum)
+    
+    ### add parallax and error to metadata
+    meta = np.append(meta, [parallax, parallax_err, zp])
 
     return (spectrum.spectrum_pk, spectrum.source_pk, norm_flux, norm_flux_err, meta, missing_photometry, missing_extinction)
     #return (spectrum.spectrum_id, norm_flux, norm_flux_err, meta)
@@ -93,25 +107,119 @@ def _inference(model, batch):
         #### astroNN prediction: K-band absolute magnitude and distance
         all_flux = np.atleast_2d(all_flux).reshape((-1, 7514))
         try:
-            fakemag, fakemag_err = model.predict(all_flux)
+            fakemag, fakemag_err, fakemag_model_err = model.predict(all_flux)
         except ValueError:
             raise
         else:
             k_mag_cor = extinction_correction(all_meta[0][0], all_meta[0][2])
             #pc, pc_err = fakemag_to_pc(fakemag, k_mag_cor, fakemag_err['total']) # for the TensorFlow version
-            pc, pc_err = fakemag_to_pc(fakemag, k_mag_cor, fakemag_err) # for the PyTorch version
+            #pc, pc_err = fakemag_to_pc(fakemag, k_mag_cor, fakemag_err) # for the PyTorch version
+
+            # For the PyTorch version
+            nn_dist, nn_dist_err = fakemag_to_pc(fakemag, k_mag_cor, fakemag_err)
+            _, nn_dist_model_err = fakemag_to_pc(fakemag, k_mag_cor, fakemag_model_err) 
+            nn_parallax, nn_parallax_err = fakemag_to_parallax(fakemag, k_mag_cor, fakemag_err) 
+            _, nn_parallax_model_err = fakemag_to_parallax(fakemag, k_mag_cor, fakemag_model_err)
+
+            # unfold Gaia parallax
+            parallax = np.atleast_1d(all_meta[0][3])
+            parallax_err = np.atleast_1d(all_meta[0][4])
+            zp = np.atleast_1d(all_meta[0][5])
+
+            #### calculate weighted parallax and distance
+            # remove astropy units
+            nn_dist = np.atleast_1d(nn_dist.value)
+            nn_dist_err = np.atleast_1d(nn_dist_err.value)
+            nn_dist_model_err = np.atleast_1d(nn_dist_model_err.value)
+            nn_parallax = np.atleast_1d(nn_parallax.value)
+            nn_parallax_err = np.atleast_1d(nn_parallax_err.value)
+            nn_parallax_model_err = np.atleast_1d(nn_parallax_model_err.value)
+
+            fakemag = np.atleast_1d(fakemag)
+            fakemag_err = np.atleast_1d(fakemag_err)
+            fakemag_model_err = np.atleast_1d(fakemag_model_err)
+
+            # set bad value to np.nan
+            bad_idx = (
+                (fakemag < 1e-4)
+                | (nn_dist == -9999.0)
+                | np.isnan(nn_dist)
+                | np.isinf(nn_dist)
+            )
+            print("+"*6, "(_inference) bad_idx:", bad_idx)
+            nn_dist[bad_idx] = np.nan
+            nn_dist_err[bad_idx] = np.nan
+            nn_dist_model_err[bad_idx] = np.nan
+            nn_parallax[bad_idx] = np.nan
+            nn_parallax_err[bad_idx] = np.nan
+            nn_parallax_model_err[bad_idx] = np.nan
+            fakemag[bad_idx] = np.nan
+            fakemag_err[bad_idx] = np.nan
+            fakemag_model_err[bad_idx] = np.nan
+
+            # calculate weighted parallax
+            deno = (1.0 / nn_parallax_model_err ** 2) + (1.0 / parallax_err ** 2)
+            weighted_parallax = (
+                (nn_parallax / nn_parallax_model_err ** 2)
+                + (parallax / parallax_err ** 2)
+            ) / deno
+            weighted_parallax_err = 1 / deno
+
+            # if one of them is -9999 or NaN, use the value from the other one
+            idx = (parallax == np.nan) & (nn_parallax != np.nan)
+            weighted_parallax[idx] = nn_parallax[idx]
+            weighted_parallax_err[idx] = nn_parallax_model_err[idx] ** 2  # still variance
+            # the other case
+            idx = (nn_parallax == np.nan) & (parallax != np.nan)
+            weighted_parallax[idx] = parallax[idx]
+            weighted_parallax_err[idx] = parallax_err[idx] ** 2  # still variance
+
+            # if both of them is -9999 or NaN, then np.nan
+            weighted_parallax[(parallax == np.nan) & (nn_parallax == np.nan)] = np.nan
+            weighted_parallax_err[(parallax == np.nan) & (nn_parallax == np.nan)] = np.nan
+
+            # need to take sqrt
+            weighted_parallax_err = np.ma.sqrt(
+                np.ma.array(
+                    weighted_parallax_err, mask=(weighted_parallax_err == np.nan), fill_value=np.nan
+                )
+            )
+
+            # change to weighted_dist
+            weighted_dist = 1000 / weighted_parallax
+            weighted_dist_err = weighted_dist * (weighted_parallax_err / weighted_parallax)
+
+            # if both of them is -9999 or NaN, then np.nan
+            weighted_dist[(parallax == np.nan) & (nn_parallax == np.nan)] = np.nan
+            weighted_dist_err[(parallax == np.nan) & (nn_parallax == np.nan)] = np.nan
     
         #### record results
         mean_t_elapsed = (time() - t_init) / len(spectrum_pks)
         for i, (spectrum_pk, source_pk) in enumerate(zip(spectrum_pks, source_pks)):
-            dist = np.atleast_1d(pc.value)[i]
-            dist_err = np.atleast_1d(pc_err.value)[i]
+            #dist = np.atleast_1d(pc.value)[i]
+            #dist_err = np.atleast_1d(pc_err.value)[i]
 
-            if dist > 10**10:
-                dist = np.nan
-            if dist_err > 10**10:
-                dist_err = np.nan
+            #if dist > 10**10:
+            #    dist = np.nan
+            #if dist_err > 10**10:
+            #    dist_err = np.nan
 
+            fakemag = float(np.atleast_1d(fakemag)[i])
+            fakemag_err = float(np.atleast_1d(fakemag_err)[i])
+            fakemag_model_err = float(np.atleast_1d(fakemag_model_err)[i])
+            nn_dist = np.atleast_1d(nn_dist)[i]
+            nn_dist_err = np.atleast_1d(nn_dist_err)[i]
+            nn_dist_model_err = np.atleast_1d(nn_dist_model_err)[i]
+            nn_parallax = np.atleast_1d(nn_parallax)[i]
+            nn_parallax_err = np.atleast_1d(nn_parallax_err)[i]
+            nn_parallax_model_err = np.atleast_1d(nn_parallax_model_err)[i]
+            gdr3_parallax_w_zp = np.atleast_1d(parallax)[i]
+            gdr3_parallax_err = np.atleast_1d(parallax_err)[i]
+            gdr3_zeropoint = np.atleast_1d(zp)[i]
+            weighted_parallax = np.atleast_1d(weighted_parallax)[i]
+            weighted_parallax_err = np.atleast_1d(weighted_parallax_err)[i]
+            weighted_dist = np.atleast_1d(weighted_dist)[i]
+            weighted_dist_err = np.atleast_1d(weighted_dist_err)[i] 
 
             output = AstroNNdist(
                 spectrum_pk=spectrum_pk,
@@ -120,11 +228,27 @@ def _inference(model, batch):
                 k_mag=all_meta[i][0],
                 ebv = all_meta[i][1],
                 a_k_mag=all_meta[i][2],
-                L_fakemag=float(np.atleast_1d(fakemag)[i]),
+                #L_fakemag=float(np.atleast_1d(fakemag)[i]),
                 #L_fakemag=float(fakemag_err['total']),
-                L_fakemag_err=float(np.atleast_1d(fakemag_err)[i]),
-                dist=dist,
-                dist_err=dist_err,
+                #L_fakemag_err=float(np.atleast_1d(fakemag_err)[i]),
+                #dist=dist,
+                #dist_err=dist_err,
+                L_fakemag=fakemag,
+                L_fakemag_err=fakemag_err,
+                L_fakemag_model_err=fakemag_model_err,
+                nn_dist=nn_dist,
+                nn_dist_err=nn_dist_err,
+                nn_dist_model_err=nn_dist_model_err,
+                nn_parallax=nn_parallax,
+                nn_parallax_err=nn_parallax_err,
+                nn_parallax_model_err=nn_parallax_model_err,
+                gdr3_parallax_w_zp=gdr3_parallax_w_zp,
+                gdr3_parallax_err=gdr3_parallax_err,
+                gdr3_zeropoint=gdr3_zeropoint,
+                weighted_parallax=weighted_parallax,
+                weighted_parallax_err=weighted_parallax_err,
+                weighted_dist=weighted_dist,
+                weighted_dist_err=weighted_dist_err
             )
             output.apply_flags(all_meta[i], missing_photometry=missing_photometrys[i], missing_extinction=missing_extinctions[i])
             #print("+"*6, "(_inference) output:", output.__data__)
@@ -251,3 +375,56 @@ def get_metadata(spectrum):
 
     #meta = dict(zip(["k_mag", "ebv", "a_k_mag"], metadata))
     return metadata, missing_photometry, missing_extinction
+
+def get_gaiadata(spectrum):
+    """
+    Get Gaia DR3 parallax for a given spectrum. Correct for zero-point parallax offset.
+
+    :param spectrum:
+        An `astra.tools.spectrum.Spectrum1D` spectrum.
+
+    :returns:
+        A two-length tuple containing the Gaia parallax and error for the given spectrum.
+    """
+    
+    zp_median = -0.03464 # median zero-point offset from Gaia DR3 using in astroNN DR17 VAC
+
+    try:
+        parallax = spectrum.source.plx
+        parallax_err = spectrum.source.e_plx
+    except:
+        return np.nan, np.nan, np.nan
+
+    if ZPT:
+        try:
+            phot_g_mean_mag = spectrum.source.phot_g_mean_mag
+            nu_eff_used_in_astrometry = spectrum.source.nu_eff_used_in_astrometry
+            pseudocolour = spectrum.source.pseudocolour
+            ecl_lat = spectrum.source.ecl_lat
+            astrometric_params_solved = spectrum.source.astrometric_params_solved
+        except:
+            zp = zp_median
+        else:
+            # calculate zero-point offset using https://pypi.org/project/gaiadr3-zeropoint/
+            if (astrometric_params_solved == 31 or astrometric_params_solved == 95) and \
+                (phot_g_mean_mag < 21) and (6 < phot_g_mean_mag):
+                zp = zpt.get_zpt(
+                    phot_g_mean_mag,
+                    nu_eff_used_in_astrometry,
+                    pseudocolour,
+                    ecl_lat,
+                    astrometric_params_solved,
+                )
+            else:
+                zp = zp_median
+    else:
+        zp = zp_median
+
+    # correct for zero-point offset
+    parallax = parallax - zp
+
+    # set negative parallax after constant offset correction to np.nan
+    if parallax < 0 or parallax > 1e10:
+        parallax, parallax_err, zp = np.nan, np.nan, np.nan
+
+    return parallax, parallax_err, zp
