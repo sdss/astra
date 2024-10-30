a916f8837648e4922231150d5ee31f69de69079a
Andy Casey <andycasey@gmail.com>
2024-10-30 09:33:01 -0600

    move the cannon help text to glossary

--- src/astra/models/the_cannon.py
+++ src/astra/models/the_cannon.py
@@ -1,6 +1,10 @@
 import datetime
+import pickle
 import numpy as np
-from peewee import (
+from peewee import fn
+from astra.utils import expand_path
+from astra.fields import (
+    BitField, PixelArray, BasePixelArrayAccessor, LogLambdaArrayAccessor,
     AutoField,
     IntegerField,
     FloatField,
@@ -8,21 +12,13 @@ from peewee import (
     ForeignKeyField,
     DateTimeField,
     BooleanField,
-    fn,
-)
-import pickle
-from astra.utils import expand_path
-from astra.models.fields import BitField, PixelArray, BasePixelArrayAccessor, LogLambdaArrayAccessor
+)    
 from astra.models.base import BaseModel
-from astra.models.fields import BitField
 from astra.models.spectrum import Spectrum
 from astra.models.source import Source
 
 from astra import __version__
 from astra.utils import log, expand_path
-from astra.glossary import Glossary
-
-
 
 
 class TheCannonPixelArrayAccessor(BasePixelArrayAccessor):
@@ -102,98 +98,98 @@ class TheCannon(BaseModel):
 
     """Stellar labels estimated using The Cannon."""
 
-    source_pk = ForeignKeyField(Source, null=True, index=True, lazy_load=False, help_text=Glossary.source_pk)
+    source_pk = ForeignKeyField(Source, null=True, index=True, lazy_load=False)
     spectrum_pk = ForeignKeyField(
         Spectrum, 
         index=True, 
         lazy_load=False,
-        help_text=Glossary.spectrum_pk
     )
     
     #> Astra Metadata
-    task_pk = AutoField(help_text=Glossary.task_pk)
-    v_astra = TextField(default=__version__, help_text=Glossary.v_astra)
-    created = DateTimeField(default=datetime.datetime.now, help_text=Glossary.created)
-    t_elapsed = FloatField(null=True, help_text=Glossary.t_elapsed)
-    t_overhead = FloatField(null=True, help_text=Glossary.t_overhead)
-    tag = TextField(default="", index=True, help_text=Glossary.tag)
+    task_pk = AutoField()
+    v_astra = TextField(default=__version__)
+    created = DateTimeField(default=datetime.datetime.now)
+    modified = DateTimeField(default=datetime.datetime.now)
+    t_elapsed = FloatField(null=True)
+    t_overhead = FloatField(null=True)
+    tag = TextField(default="", index=True)
     
     #> Stellar Labels
-    teff = FloatField(default=np.nan, help_text=Glossary.teff)
-    e_teff = FloatField(default=np.nan, help_text=Glossary.e_teff)
-    logg = FloatField(default=np.nan, help_text=Glossary.logg)
-    e_logg = FloatField(default=np.nan, help_text=Glossary.e_logg)
-    fe_h = FloatField(default=np.nan, help_text=Glossary.fe_h)
-    e_fe_h = FloatField(default=np.nan, help_text=Glossary.e_fe_h)
-    v_micro = FloatField(default=np.nan, help_text=Glossary.v_micro)
-    e_v_micro = FloatField(default=np.nan, help_text=Glossary.e_v_micro)
-    v_macro = FloatField(default=np.nan, help_text=Glossary.v_macro)
-    e_v_macro = FloatField(default=np.nan, help_text=Glossary.e_v_macro)
+    teff = FloatField(default=np.nan)
+    e_teff = FloatField(default=np.nan)
+    logg = FloatField(default=np.nan)
+    e_logg = FloatField(default=np.nan)
+    fe_h = FloatField(default=np.nan)
+    e_fe_h = FloatField(default=np.nan)
+    v_micro = FloatField(default=np.nan)
+    e_v_micro = FloatField(default=np.nan)
+    v_macro = FloatField(default=np.nan)
+    e_v_macro = FloatField(default=np.nan)
     
     #> Chemical Abundances
-    c_fe = FloatField(default=np.nan, help_text="[C/Fe] abundance ratio")
-    e_c_fe = FloatField(default=np.nan, help_text="Error on [C/Fe] abundance ratio")
-    n_fe = FloatField(default=np.nan, help_text="[N/Fe] abundance ratio")
-    e_n_fe = FloatField(default=np.nan, help_text="Error on [N/Fe] abundance ratio")
-    o_fe = FloatField(default=np.nan, help_text="[O/Fe] abundance ratio")
-    e_o_fe = FloatField(default=np.nan, help_text="Error on [O/Fe] abundance ratio")
-    na_fe = FloatField(default=np.nan, help_text="[Na/Fe] abundance ratio")
-    e_na_fe = FloatField(default=np.nan, help_text="Error on [Na/Fe] abundance ratio")
-    mg_fe = FloatField(default=np.nan, help_text="[Mg/Fe] abundance ratio")
-    e_mg_fe = FloatField(default=np.nan, help_text="Error on [Mg/Fe] abundance ratio")
-    al_fe = FloatField(default=np.nan, help_text="[Al/Fe] abundance ratio")
-    e_al_fe = FloatField(default=np.nan, help_text="Error on [Al/Fe] abundance ratio")
-    si_fe = FloatField(default=np.nan, help_text="[Si/Fe] abundance ratio")
-    e_si_fe = FloatField(default=np.nan, help_text="Error on [Si/Fe] abundance ratio")
-    s_fe = FloatField(default=np.nan, help_text="[S/Fe] abundance ratio")
-    e_s_fe = FloatField(default=np.nan, help_text="Error on [S/Fe] abundance ratio")
-    k_fe = FloatField(default=np.nan, help_text="[K/Fe] abundance ratio")
-    e_k_fe = FloatField(default=np.nan, help_text="Error on [K/Fe] abundance ratio")
-    ca_fe = FloatField(default=np.nan, help_text="[Ca/Fe] abundance ratio")
-    e_ca_fe = FloatField(default=np.nan, help_text="Error on [Ca/Fe] abundance ratio")
-    ti_fe = FloatField(default=np.nan, help_text="[Ti/Fe] abundance ratio")
-    e_ti_fe = FloatField(default=np.nan, help_text="Error on [Ti/Fe] abundance ratio")
-    v_fe = FloatField(default=np.nan, help_text="[V/Fe] abundance ratio")
-    e_v_fe = FloatField(default=np.nan, help_text="Error on [V/Fe] abundance ratio")
-    cr_fe = FloatField(default=np.nan, help_text="[Cr/Fe] abundance ratio")
-    e_cr_fe = FloatField(default=np.nan, help_text="Error on [Cr/Fe] abundance ratio")
-    mn_fe = FloatField(default=np.nan, help_text="[Mn/Fe] abundance ratio")
-    e_mn_fe = FloatField(default=np.nan, help_text="Error on [Mn/Fe] abundance ratio")
-    ni_fe = FloatField(default=np.nan, help_text="[Ni/Fe] abundance ratio")
-    e_ni_fe = FloatField(default=np.nan, help_text="Error on [Ni/Fe] abundance ratio")
+    c_fe = FloatField(default=np.nan)
+    e_c_fe = FloatField(default=np.nan)
+    n_fe = FloatField(default=np.nan)
+    e_n_fe = FloatField(default=np.nan)
+    o_fe = FloatField(default=np.nan)
+    e_o_fe = FloatField(default=np.nan)
+    na_fe = FloatField(default=np.nan)
+    e_na_fe = FloatField(default=np.nan)
+    mg_fe = FloatField(default=np.nan)
+    e_mg_fe = FloatField(default=np.nan)
+    al_fe = FloatField(default=np.nan)
+    e_al_fe = FloatField(default=np.nan)
+    si_fe = FloatField(default=np.nan)
+    e_si_fe = FloatField(default=np.nan)
+    s_fe = FloatField(default=np.nan)
+    e_s_fe = FloatField(default=np.nan)
+    k_fe = FloatField(default=np.nan)
+    e_k_fe = FloatField(default=np.nan)
+    ca_fe = FloatField(default=np.nan)
+    e_ca_fe = FloatField(default=np.nan)
+    ti_fe = FloatField(default=np.nan)
+    e_ti_fe = FloatField(default=np.nan)
+    v_fe = FloatField(default=np.nan)
+    e_v_fe = FloatField(default=np.nan)
+    cr_fe = FloatField(default=np.nan)
+    e_cr_fe = FloatField(default=np.nan)
+    mn_fe = FloatField(default=np.nan)
+    e_mn_fe = FloatField(default=np.nan)
+    ni_fe = FloatField(default=np.nan)
+    e_ni_fe = FloatField(default=np.nan)
     
     #> Summary Statistics
-    chi2 = FloatField(default=np.nan, help_text=Glossary.chi2)
-    rchi2 = FloatField(default=np.nan, help_text=Glossary.rchi2)
+    chi2 = FloatField(default=np.nan)
+    rchi2 = FloatField(default=np.nan)
 
     #> Formal uncertainties
-    raw_e_teff = FloatField(default=np.nan, help_text=Glossary.raw_e_teff)
-    raw_e_logg = FloatField(default=np.nan, help_text=Glossary.raw_e_logg)
-    raw_e_fe_h = FloatField(default=np.nan, help_text=Glossary.raw_e_fe_h)
-    raw_e_v_micro = FloatField(default=np.nan, help_text=Glossary.raw_e_v_micro)
-    raw_e_v_macro = FloatField(default=np.nan, help_text=Glossary.raw_e_v_macro)
-    raw_e_c_fe = FloatField(default=np.nan, help_text="Raw error on [C/Fe] abundance ratio")
-    raw_e_n_fe = FloatField(default=np.nan, help_text="Raw error on [N/Fe] abundance ratio")
-    raw_e_o_fe = FloatField(default=np.nan, help_text="Raw error on [O/Fe] abundance ratio")
-    raw_e_na_fe = FloatField(default=np.nan, help_text="Raw error on [Na/Fe] abundance ratio")
-    raw_e_mg_fe = FloatField(default=np.nan, help_text="Raw error on [Mg/Fe] abundance ratio")
-    raw_e_al_fe = FloatField(default=np.nan, help_text="Raw error on [Al/Fe] abundance ratio")
-    raw_e_si_fe = FloatField(default=np.nan, help_text="Raw error on [Si/Fe] abundance ratio")
-    raw_e_s_fe = FloatField(default=np.nan, help_text="Raw error on [S/Fe] abundance ratio")
-    raw_e_k_fe = FloatField(default=np.nan, help_text="Raw error on [K/Fe] abundance ratio")
-    raw_e_ca_fe = FloatField(default=np.nan, help_text="Raw error on [Ca/Fe] abundance ratio")
-    raw_e_ti_fe = FloatField(default=np.nan, help_text="Raw error on [Ti/Fe] abundance ratio")
-    raw_e_v_fe = FloatField(default=np.nan, help_text="Raw error on [V/Fe] abundance ratio")
-    raw_e_cr_fe = FloatField(default=np.nan, help_text="Raw error on [Cr/Fe] abundance ratio")
-    raw_e_mn_fe = FloatField(default=np.nan, help_text="Raw error on [Mn/Fe] abundance ratio")
-    raw_e_ni_fe = FloatField(default=np.nan, help_text="Raw error on [Ni/Fe] abundance ratio")
+    raw_e_teff = FloatField(default=np.nan)
+    raw_e_logg = FloatField(default=np.nan)
+    raw_e_fe_h = FloatField(default=np.nan)
+    raw_e_v_micro = FloatField(default=np.nan)
+    raw_e_v_macro = FloatField(default=np.nan)
+    raw_e_c_fe = FloatField(default=np.nan)
+    raw_e_n_fe = FloatField(default=np.nan)
+    raw_e_o_fe = FloatField(default=np.nan)
+    raw_e_na_fe = FloatField(default=np.nan)
+    raw_e_mg_fe = FloatField(default=np.nan)
+    raw_e_al_fe = FloatField(default=np.nan)
+    raw_e_si_fe = FloatField(default=np.nan)
+    raw_e_s_fe = FloatField(default=np.nan)
+    raw_e_k_fe = FloatField(default=np.nan)
+    raw_e_ca_fe = FloatField(default=np.nan)
+    raw_e_ti_fe = FloatField(default=np.nan)
+    raw_e_v_fe = FloatField(default=np.nan)
+    raw_e_cr_fe = FloatField(default=np.nan)
+    raw_e_mn_fe = FloatField(default=np.nan)
+    raw_e_ni_fe = FloatField(default=np.nan)
 
     #> Metadata
-    ier = IntegerField(default=-1, help_text="Returned state from optimizer")
-    nfev = IntegerField(default=-1, help_text="Number of function evaluations")
-    x0_index = IntegerField(default=-1, help_text="Index of initial guess used")
-    result_flags = BitField(default=0, help_text="Result flags")
-    flag_fitting_failure = result_flags.flag(2**0, "Fitting failure")
+    ier = IntegerField(default=-1)
+    nfev = IntegerField(default=-1)
+    x0_index = IntegerField(default=-1)
+    result_flags = BitField(default=0)
+    flag_fitting_failure = result_flags.flag(2**0, help_text="Fitting failure")
     
     #> Spectral data
     wavelength = PixelArray(
@@ -203,10 +199,9 @@ class TheCannon(BaseModel):
             cdelt=6e-6,
             naxis=8575,
         ),
-        help_text=Glossary.wavelength
     )
-    model_flux = TheCannonPixelArray(help_text=Glossary.model_flux)
-    continuum = TheCannonPixelArray(help_text=Glossary.continuum)    
+    model_flux = TheCannonPixelArray()
+    continuum = TheCannonPixelArray()
     
     @property
     def intermediate_output_path(self):
