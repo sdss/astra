23d5502778c53897a5c06e3904a7b583f034c68b
imedan <ilija.medan@gmail.com>
2026-09-09 06:52:47 -0600

    add catches in debugger for if PBS not set or another OSError

--- src/astra/pipelines/aspcap/debugger.py
+++ src/astra/pipelines/aspcap/debugger.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 RAND = np.random.randint(0, 1000)
@@ -6,5 +7,16 @@ from astra.utils import expand_path
 HOSTNAME = gethostname()
 
 def debugger(*foo):
-    with open(expand_path(f"$PBS/{HOSTNAME}-{RAND}.log"), "a") as fp:
-        fp.write(" ".join(map(str, foo)) + "\n")
+    # `debugger` is called from inside exception handlers (e.g. `_safe_pre_process_ferre`),
+    # so it must never raise: a failure here would mask the error being reported. $PBS is
+    # unset outside of a cluster environment (CI, a laptop), in which case `expand_path`
+    # leaves the literal "$PBS" and the open() below would fail with FileNotFoundError.
+    path = expand_path(f"$PBS/{HOSTNAME}-{RAND}.log")
+    if "$" in path:
+        return
+    try:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "a") as fp:
+            fp.write(" ".join(map(str, foo)) + "\n")
+    except OSError:
+        return
